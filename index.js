0c5b2cb335ecaa2a60301125679c7526cffe5634
ahzamir <ahmadzamiryousofi1397@gmail.com>
2022-04-14 01:47:48 +0430

    add the data to the see-project with js

--- index.js
+++ index.js
@@ -23,33 +23,48 @@ function contactSection() {
   window.location = `${window.location.pathname}#contact`;
 }
 const projects = [{
-  name: 'Profesional Art  Printing Data',
+  name: 'Profesional Art  Printing Data ',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg",
+  languages: []
 },
 {
-  name: 'Data Dashboard Healthcare',
+  name: 'Data Dashboard Healthcare ',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg"
 },
 {
-  name: 'Website Protfolio',
+  name: 'Website Protfolio ',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg"
 },
 {
-  name: 'Profesional Art  Printing Data',
+  name: 'Profesional Art  Printing Data ',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg"
 },
 {
-  name: 'Data Dashboard Healthcare',
+  name: 'Data Dashboard Healthcare ',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg"
 },
 {
-  name: 'Website Protfolio',
+  name: 'Website Protfolio 6',
   description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry s standard',
+  Image: "images/snapshoot-portfolio.svg"
 }];
+function openProject(event) {
+  const id = event.target.id;
+  const project = projects[id];
+  document.getElementById('exampleModalLabel').innerHTML = project.name;
+  document.getElementById('pro-description').innerHTML = project.description;
+  document.getElementById('pro-img').src = project.Image;
+}
 function displayProjects() {
   const projectPart = document.getElementById('cards');
   const cards = document.createElement('div');
   cards.classList.add('cards-pro');
+  let id = 0;
   projects.forEach((project) => {
     const card = `<div class="second-project project-back">
       <h2 class="project-heading">${project.name}</h2>
@@ -59,18 +74,18 @@ function displayProjects() {
         <li>bootstrap</li>
         <li>Ruby</li>
       </ul>
-    <a class="link-project2 link-project2-desktop" type="button" data-toggle="modal" data-target="#exampleModal">
-      <div class="see-project2">See Project</div>
+    <a class="link-project2 link-project2-desktop" id=${id} type="button" data-toggle="modal" data-target="#exampleModal">
+      <div class="see-project2" id=${id}>See Project</div>
     </a>
     </div>
-    <a class="link-project2 link-project2-mobile" type="button" data-toggle="modal" data-target="#exampleModal">
-    <div class="see-project2">See Project</div>
+    <a class="link-project2 link-project2-mobile" id=${id} type="button" data-toggle="modal" data-target="#exampleModal">
+    <div class="see-project2 id=${id}">See Project</div>
     </a>
     `;
     cards.insertAdjacentHTML('beforeend', card);
+    id++;
   });
   projectPart.appendChild(cards);
-  
 }
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('hamberger').addEventListener('click', displayContent);
@@ -79,4 +94,8 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.about').addEventListener('click', aboutSection);
   document.querySelector('.contact').addEventListener('click', contactSection);
   displayProjects();
+  const projectLinks = document.querySelectorAll('.link-project2');
+  projectLinks.forEach((projectLink) => {
+    projectLink.addEventListener('click', openProject);
+  })
 });
